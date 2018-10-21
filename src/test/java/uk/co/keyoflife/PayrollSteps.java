6b7c70d22f3750d61ac9836820e46be3475757be
Ronan O'Connell <roconnell@sorensonmedia.com>
2018-10-21 12:53:55 +0100

    First test implemented but failing.
    Switched to chrome as the firefox driver needed something called gecko
    driver.
    
    Close browser after the tests are complete.
    Updated test and code to pass basic test from which I hope to build a CI
    build

---
+++ src/test/java/uk/co/keyoflife/PayrollSteps.java
@@ -0,0 +1,29 @@
+package uk.co.keyoflife;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class PayrollSteps {
+  private final WebDriver driver = new ChromeDriver();
+	
+  @Given("I visit the payroll web site")
+  public void given() throws Throwable {
+	  driver.get("http://localhost:8080");
+  }
+
+  @Then("The Page title should be {string}")
+  public void checkTitle(String expectedPageTitle) throws Throwable {
+	 assertEquals(expectedPageTitle, driver.getTitle()); 
+  }
+  
+  @After()
+  public void closeBrowser() {
+    driver.quit();
+  }
+}
