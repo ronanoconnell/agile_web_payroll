34519709495c03d6a829ab7902de8b95400a8913
Ronan O'Connell <roconnell@sorensonmedia.com>
2018-10-21 11:58:57 +0100

    Addition of cucumbmer dependcies and autogenerated feature file to prove that build works

---
+++ src/test/java/uk/co/keyoflife/RunCukesTest.java
@@ -0,0 +1,10 @@
+package uk.co.keyoflife;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukesTest {
+}
