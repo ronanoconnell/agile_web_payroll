f348351e30871d1ce9ddf7ca4eea57d64edb7ffc
ronan <ronanoconnell1971@gmail.com>
2019-03-16 15:17:57 +0000

    Tidy up unused throws statements.

--- src/test/java/uk/co/keyoflife/PayrollSteps.java
+++ src/test/java/uk/co/keyoflife/PayrollSteps.java
@@ -13,12 +13,12 @@ public class PayrollSteps {
   private final WebDriver driver = new ChromeDriver();
 	
   @Given("I visit the payroll web site")
-  public void given() throws Throwable {
+  public void given() {
 	  driver.get("http://localhost:8080");
   }
 
   @Then("The Page title should be {string}")
-  public void checkTitle(String expectedPageTitle) throws Throwable {
+  public void checkTitle(String expectedPageTitle) {
 	 assertEquals(expectedPageTitle, driver.getTitle()); 
   }
   
