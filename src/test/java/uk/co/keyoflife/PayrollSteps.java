ef967455e5c17d006c4f6c680d11e8c0ba8bf4e8
ronanoconnell <ronanoconnell1971@gmail.com>
2019-04-14 07:59:46 +0100

    01 Calculate Salary Pay Implemented the step definitions then created the payroll calculation form with matching ids.

--- src/test/java/uk/co/keyoflife/PayrollSteps.java
+++ src/test/java/uk/co/keyoflife/PayrollSteps.java
@@ -5,11 +5,11 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class PayrollSteps {
   private final WebDriver driver = new ChromeDriver();
@@ -31,24 +31,29 @@ public class PayrollSteps {
 
   @Given("An employee named {string}")
   public void anEmployeeNamed(String employeeName) {
-
+      driver.findElement(By.id("employeeName")).sendKeys(employeeName);
   }
 
   @And("is paid £{double} per annum")
   public void paidPerAnnum(Double annualSalary) {
+      final String annualSalaryAsString = annualSalary.toString();
+      driver.findElement(By.id("employeeSalary")).sendKeys(annualSalaryAsString);
   }
 
   @And("has an annual Tax credit of £{double}")
   public void hasAnAnnualTaxFreeCrediteOf(Double annualTaxCredit) {
-
+      driver.findElement(By.id("employeeTaxCredit")).sendKeys(annualTaxCredit.toString());
   }
 
   @When("their pay is calculated")
   public void therePayIsCalculated() {
+      driver.findElement(By.id("calculation-form")).submit();
   }
 
   @Then("their net pay is £{double}")
-  public void theirNetPayIs(double netPay) {
-      fail("Test not implemented");
+  public void theirNetPayIs(Double expectedNetPay) {
+      final String netPayAsString = driver.findElement(By.id("netPay")).getText();
+      final Double calculatedNetPay = netPayAsString.isEmpty() ? 0.0 : new Double(netPayAsString);
+      assertEquals(expectedNetPay, calculatedNetPay);
   }
 }
